b1f0021554d2efbbd418c38e135146fa065b7171
Joe <spizjo01@gmail.com>
2022-10-10 17:41:58 -0600

    commit tree 9f71430abe107a8fa1e72ed498b18b02291e9875

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait
+
+    def clickbait
+        unless title != "True Facts"
+            errors.add(:title, "Nope")
+        end
+    end
+
 end
